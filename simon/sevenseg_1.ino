38877103bebbb505f915cfdc0ff990d86e0c16ff
shanmsun <ymsun007@gmail.com>
2018-09-17 23:46:31 -0400

    Update sevenseg_1.ino
    Code complete.
    TODOs: 
    1) add ending 'winning' sequence when max games completed
    2)  Minimize flushing sequences in the beginning

--- simon/sevenseg_1.ino
+++ simon/sevenseg_1.ino
@@ -1,24 +1,9 @@
 #include <EEPROM.h>
-#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+#include <Wire.h>
 #include "Adafruit_GFX.h"
 #include "Adafruit_LEDBackpack.h"
-const int BUZZER_PIN = 9; //Digital Pin
-const int BUZZER_FEQ = 1000; //HZ (cycles per second)
-const int BUZZER_DUR = 500; // length of tome played
-const int c = 261;
-const int e = 329;
-const int g = 391;
-const int cH = 523;
-const int eH = 659;
-const int feq[5] = {c, e, g, cH, eH};
-void buzzer_test() {
-  for (int i=4; i>=0; i--){
-    tone(BUZZER_PIN, feq[i], BUZZER_DUR);
-    delay(200);
-  }
-}
-Adafruit_7segment matrix = Adafruit_7segment();
-Adafruit_7segment matrix2 = Adafruit_7segment();
+
+//Buttons
 unsigned long buttonPin[6] = {A5,10,11,12,13,8};    
 unsigned long ledPin[6] = {2,5,7,6,4,3};      
 unsigned long button0[7];
@@ -28,57 +13,52 @@ unsigned long button3[7];
 unsigned long button4[7];
 unsigned long button5[7]; //StartButton
 unsigned long * Buttons[6] = {button0,button1,button2,button3,button4,button5};
+unsigned long debounceDelay = 50;    // increase if needed
+
+//Constants
+int PRESET = 5;
+const int MAXGAME = 125;
+int address = 0;  //EEPROM address (0,1,2,3 4 bytes in total)
+//Buzzer 
+const int BUZZER_PIN = 9; //Digital Pin
+const int BUZZER_FEQ = 1000; //HZ (cycles per second)
+const int BUZZER_DUR = 500; // length of the notes
+const int c = 261;
+const int e = 329;
+const int g = 391;
+const int cH = 523;
+const int eH = 659;
+const int feq[5] = {c, e, g, cH, eH};
+
+//Global Variables
 bool STARTGAME = false;
 bool FAILED = false;
 bool RESET = false;
 bool SEQED = false;
 bool WAIT = false;
 bool LIGHT = false;
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-int led_fadeAmount=5; 
-unsigned long led_delay;
-unsigned long last_active;
-unsigned long sequence_delay;
-int sequence[100];
+//Score
+unsigned long HighScore = 0;
+unsigned long currScore = 0;
+//Sequence
+int sequence[MAXGAME];
 int currLen = 0;
 int currNote = 0;
 int currPressed = 0;
 int currGame = 0;
-unsigned long currScore = 0;
-const int MAXGAME = 100;
-unsigned long HighScore = 0;
-int address = 0;
-byte value = 0;
-int PRESET = 10;
-
-void printScore() {
-  matrix.print(currScore, DEC);
-  matrix.writeDisplay();
-  if (currScore > HighScore){
-    HighScore = (int)currScore;
-    printHighScore();
-  }
-}
-
-void printHighScore(){
-  matrix2.print(HighScore, DEC);
-  matrix2.writeDisplay();
-  unsigned long value1 = HighScore;
-  byte four1 = (value1 & 0xFF);
-  byte three1 = ((value1 >> 8) & 0xFF);
-  byte two1 = ((value1 >> 16) & 0xFF);
-  byte one1 = ((value1 >> 24) & 0xFF);
-  EEPROM.write(address, four1);
-  EEPROM.write(address + 1, three1);
-  EEPROM.write(address + 2, two1);
-  EEPROM.write(address + 3, one1);
-}
+unsigned long last_active;
+unsigned long sequence_delay;
+//LED Fade
+int led_fadeAmount=5; 
+unsigned long led_delay;
+// 7 Seg Display
+Adafruit_7segment matrix = Adafruit_7segment();
+Adafruit_7segment matrix2 = Adafruit_7segment();
 
 void setup() {
-  matrix.begin(0x70); // RED Your Score
-  matrix2.begin(0x71); // GREEN A0 High Score
+  matrix.begin(0x70);   // RED - Your Score
+  matrix2.begin(0x71);  // GREEN - (A0) High Score
   Serial.begin(9600);
-  //delay(10000); //wait till things are settled?
   for (int i=0; i <= 5; i++){
     initializeButton( (Buttons[i]), buttonPin[i], ledPin[i]);
   }
@@ -93,32 +73,12 @@ void setup() {
   currScore = 0;
   currGame = 0;
   currLen = 0;
-  testeeprom();
-  //HighScore = ((int)value) % 9999;
+  testeeprom(); //Read high score from eeprom
   printScore();
-  buzzer_test();
+  buzzerScale();
 }
 
-void testeeprom(){
-  //CLEAR EEPROM
-//  long value1 = 0000;
-//  byte four1 = (value1 & 0xFF);
-//  byte three1 = ((value1 >> 8) & 0xFF);
-//  byte two1 = ((value1 >> 16) & 0xFF);
-//  byte one1 = ((value1 >> 24) & 0xFF);
-//  EEPROM.write(address, four1);
-//  EEPROM.write(address + 1, three1);
-//  EEPROM.write(address + 2, two1);
-//  EEPROM.write(address + 3, one1);
-  long four = EEPROM.read(address);
-  long three = EEPROM.read(address + 1);
-  long two = EEPROM.read(address + 2);
-  long one = EEPROM.read(address + 3);
-  HighScore = ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
-  Serial.print(8888);
-  Serial.print(HighScore);
-  Serial.print(8888);
-}
+
 
 void loop() {
   STARTGAME = RESET; 
@@ -128,33 +88,32 @@ void loop() {
   (Buttons[5])[3] = 0; 
   led_fadeAmount=5;
   (Buttons[5])[6] = 0;
-
-  //REFRESH SEQUENCE
+  
+  //REFRESH FIX-TODO
   if (PRESET >0){
     PRESET--;
     STARTGAME = true;
   }
-  
+
+  //IDLE
   while(!STARTGAME){
-    led_fade_test(Buttons[5]);
+    ledFade(Buttons[5]);
     buttonToggled_PWM(Buttons[5]);
     STARTGAME = ((Buttons[5])[3]); //pressed
   }
-  //Reset all buttons
+  
+  //Reset Buttons
   for (int i=0; i <= 4; i++){
       (Buttons[i])[6] = 0;
       digitalWrite((Buttons[i])[5], (Buttons[i])[6]);    
   }
   //Reset Game
   currLen = 0;
-  for (int i=0; i < 100; i++){
-    sequence[i]=6;
+  for (int i=0; i < MAXGAME; i++){
+    sequence[i]=6;  //6 is invalid
   }
-  
-  
-  //(Buttons[2])[6] = 1;
-  //digitalWrite((Buttons[2])[5], (Buttons[2])[6]); 
-  //delay(3000);
+
+  //Begin Game
   start();
 }
 
@@ -198,7 +157,12 @@ void start(){
 }
 
 void extendOne(){
-  sequence[currLen] = random(0,5);
+  int temp;
+  temp = random(0,6);
+  if (temp >= 5){
+    temp = 3;
+  }
+  sequence[currLen] = (int)temp;
   currLen++;
   LIGHT = true;
   WAIT = false;
@@ -213,7 +177,7 @@ void endGame(){
     (Buttons[i])[6] = 1;
     digitalWrite((Buttons[i])[5], (Buttons[i])[6]);
   }
-  buzzer_test();
+  buzzerScale();
   delay(1000);
   for (int i=0; i <= 4; i++){
     (Buttons[i])[6] = 0;
@@ -225,7 +189,7 @@ void endSequence(){
   extendOne();
   currGame++;
   currNote = 0;
-  delay(200);
+  delay(500);
 }
 
 void waitSequence(){
@@ -282,8 +246,8 @@ void waitSequence(){
 void lightSequence(){
   int note;
   //sequence_delay = millis();
-  if ((currLen > 0) && (currLen <=100) && LIGHT){
-    if (millis() -  sequence_delay >= 1500){
+  if ((currLen > 0) && (currLen <=MAXGAME) && LIGHT){
+    if (millis() -  sequence_delay >= 700){
       if (currNote <= currLen){
         currNote++;
         note = sequence[currNote-1];
@@ -304,7 +268,7 @@ void lightSequence(){
         }
       }
     }
-    else if (millis() -  sequence_delay >= 1200){
+    else if (millis() -  sequence_delay >= 500){
       note = sequence[currNote-1];
       if(note!=6){
         (Buttons[note])[6] = 0;
@@ -331,7 +295,7 @@ void lightSequence(){
         }
       }
   }
-  //else{ what happens if reaches 100
+  //else{ TODO FIX - ending 'wining' sequence for completing max games
   // When all sequence displayed
   if (currNote > currLen){
     WAIT = true;
@@ -402,7 +366,8 @@ void initializeButton(unsigned long *buttonObject, unsigned long pinID, unsigned
   pinMode((int)ledPin, OUTPUT);
   digitalWrite((int)ledPin, (int)buttonObject[6]);
 }
-void led_fade_test(unsigned long *buttonObject)  { 
+
+void ledFade(unsigned long *buttonObject)  { 
   if (millis() - led_delay >= 30){
     led_delay = millis();
     analogWrite(buttonObject[5], buttonObject[6]);    
@@ -412,4 +377,55 @@ void led_fade_test(unsigned long *buttonObject)  {
       led_fadeAmount = -led_fadeAmount ; 
     }     
   }                            
+}
+
+void buzzerScale() {
+  for (int i=4; i>=0; i--){
+    tone(BUZZER_PIN, feq[i], BUZZER_DUR);
+    delay(200);
+  }
+}
+
+void printScore() {
+  currScore = min(9999,currScore);  //cast both scores to 9999 here
+  matrix.print(currScore, DEC);
+  matrix.writeDisplay();
+  if (currScore > HighScore){
+    HighScore = currScore;
+    printHighScore();
+  }
+}
+
+void printHighScore(){
+  matrix2.print(HighScore, DEC);
+  matrix2.writeDisplay();
+  unsigned long value1 = HighScore;
+  byte four1 = (value1 & 0xFF);
+  byte three1 = ((value1 >> 8) & 0xFF);
+  byte two1 = ((value1 >> 16) & 0xFF);
+  byte one1 = ((value1 >> 24) & 0xFF);
+  EEPROM.write(address, four1);
+  EEPROM.write(address + 1, three1);
+  EEPROM.write(address + 2, two1);
+  EEPROM.write(address + 3, one1);
+}
+
+void testeeprom(){
+//  CLEAR EEPROM
+//  long value1 = 0000;
+//  byte four1 = (value1 & 0xFF);
+//  byte three1 = ((value1 >> 8) & 0xFF);
+//  byte two1 = ((value1 >> 16) & 0xFF);
+//  byte one1 = ((value1 >> 24) & 0xFF);
+//  EEPROM.write(address, four1);
+//  EEPROM.write(address + 1, three1);
+//  EEPROM.write(address + 2, two1);
+//  EEPROM.write(address + 3, one1);
+  long four = EEPROM.read(address);
+  long three = EEPROM.read(address + 1);
+  long two = EEPROM.read(address + 2);
+  long one = EEPROM.read(address + 3);
+  HighScore = ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
+  Serial.print(HighScore);
+  printHighScore();
 }
